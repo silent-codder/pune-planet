1bcba98006a334b030d6e73a175b6746f9b3f618
silent-codder <ajinkyashinde9799@gmail.com>
2021-08-14 23:47:46 +0530

    new code update

---
+++ app/src/main/java/com/cctpl/puneplanet/Fragments/DocDetailsFragment.java
@@ -0,0 +1,24 @@
+package com.cctpl.puneplanet.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.cctpl.puneplanet.R;
+
+
+public class DocDetailsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_doc_details, container, false);
+
+        return view;
+    }
+}
